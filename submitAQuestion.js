3f98ba6027a6d0d4e882141d6ee4f58186ab9caf
JRProgramming <ramirezjohnny@icloud.com>
2018-02-28 08:50:53 -0500

    Update submitAQuestion.js

--- submitAQuestion.js
+++ submitAQuestion.js
@@ -1,21 +1,25 @@
 function submitQuestion()
 {
 alert("You have submited a question. Great job!")
-
-
-var save = firebase.database().ref('user/').push({
-    username: name,
-    email: email
+var questionName = document.getElementById("questionName").value;
+var correctAnswer = document.getElementById("correctAnswer").value;
+var wrongAnswer1 = document.getElementById("wrongAnswer1").value;
+var wrongAnswer2 = document.getElementById("wrongAnswer2").value;
+    
+var save = firebase.database().ref('questions/').push({
+    questionName: questionName,
+    correctAnswer: correctAnswer,
+    wrongAnswer1: wrongAnswer1,
+    wrongAnswer2: wrongAnswer2
   });
 var id = save.key
-firebase.database().ref('user/' + id).update({
-identification: id
+firebase.database().ref('questions/' + id).update({
+    identification: id
 })
     firebase.database().ref('Completed/').set("Data is saved", function(error) {
         if (error) {
             alert("Data could not be saved." + error);
         } else {
-            localStorage.setItem("login", true)
             location.href = "https://jrprogramming.github.io/BBTrivia/homePage"
         }
       });
