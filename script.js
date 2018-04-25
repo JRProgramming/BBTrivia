b278942259c64e1c0610b75635729afb64272fe8
JRProgramming <ramirezjohnny@icloud.com>
2018-04-25 16:06:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var isEligibleEmail = true;
+
 function register(){
   var name = document.getElementById("nameTextField").value
   var email = document.getElementById("schoolEmailTextField").value
@@ -25,29 +27,54 @@ function register(){
           }
           if(isAMember != true)
           {
-            var save = firebase.database().ref('user/').push({
-                username: name,
-                email: email,
-                eligible: true
-            });
-            var id = save.key
-            firebase.database().ref('user/' + id).update({
-              identification: id
-            })
-            firebase.database().ref('Completed/').set("Data is saved", function(error) {
-                if (error) {
-                    alert("Data could not be saved." + error);
-                } else {
-                  sessionStorage.setItem("login", true)
-                  location.href = "homePage.html"
-                }
-            });
-        }
-        else
-        {
-          //The user already has an account right here.
-          location.href = "homePage.html"
-        }
+            var reversedBlindBrook = "gro.koorbdnilb"
+            var reversedEmail = ""
+            for(i=0;i<email.length;i++)
+            {
+              reversedEmail = email[i] + reversedEmail
+            }
+            for(i=0;i<14;i++)
+            {
+              if(reversedEmail[i] != reversedBlindBrook[i])
+              {
+                isEligibleEmail = false
+              }
+            }
+            if(isEligibleEmail != false)
+            {
+              var save = firebase.database().ref('user/').push({
+                  username: name,
+                  email: email,
+                  eligible: true,
+                  questionsAnswered: 0,
+                  totalQuestions: 0,
+                  gamesWon: 0,
+                  totalGames: 0
+              });
+              var id = save.key
+              firebase.database().ref('user/' + id).update({
+                identification: id
+              })
+              firebase.database().ref('Completed/').set("Data is saved", function(error) {
+                  if (error) {
+                      alert("Data could not be saved." + error);
+                  } else {
+                    sessionStorage.setItem("login", true)
+                    location.href = "homePage.html"
+                  }
+              });
+            }
+            else {
+              alert("I'm sorry but this website only allows people with Blind Brook emails to have access to this site.")
+              name = ""
+              email = ""
+            }
+          }
+          else
+          {
+            //The user already has an account right here.
+            location.href = "homePage.html"
+          }
       }
     })
   }
@@ -55,14 +82,17 @@ function register(){
   {
     if(name == "" && email == "")
     {
+      //Checks to see if both of the textfields are filled in
       alert("Both textfields are empty. Please type in your full name and your email.")
     }
     else if(name == "")
     {
+      //Will send an alert if the name textfield is blank
       alert("You didn't type in your name. Please type in your first and last name.")
     }
     else
     {
+      //Will send an alert if the email textfield is blank
       alert("You didn't type in your school email. Please do that right now.")
     }
   }
