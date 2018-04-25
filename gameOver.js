cf4b78cede8084921dc41ec4ff4c9aceedea81ed
JRProgramming <ramirezjohnny@icloud.com>
2018-04-25 16:20:37 -0400

    Update gameOver.js

--- gameOver.js
+++ gameOver.js
@@ -1,4 +1,14 @@
 function homePage()
 {
-  location.href = "homePage.html"
+  location.href = "homepage.html"
+  //If the user decides to go to the homepage
+  //When they click a button, they will be sent to back to the homepage
+}
+window.onload = function()
+{
+  if(sessionStorage.getItem("BBTrivianame") == null)
+  {
+    location.href = "index.html"
+  }
+  //Checks to see if the user logged in
 }
