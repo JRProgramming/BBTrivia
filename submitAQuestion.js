ef8a042211b90e2a4377f5b6e5d2f29f3b2ef2cc
JRProgramming <ramirezjohnny@icloud.com>
2018-04-25 17:30:53 -0400

    Update submitAQuestion.js

--- submitAQuestion.js
+++ submitAQuestion.js
@@ -4,6 +4,9 @@ function submitQuestion()
   var correctAnswer = document.getElementById("correctAnswer").value;
   var wrongAnswer1 = document.getElementById("wrongAnswer1").value;
   var wrongAnswer2 = document.getElementById("wrongAnswer2").value;
+  var previousNumberOfQuestionsSubmitted = 0
+  var identity = null
+  var userArray = []
     if(questionName != "" && correctAnswer != "" && wrongAnswer1 != "" && wrongAnswer2 != "")
     {
         //Makes sure that none of the textfields are blank
@@ -22,10 +25,46 @@ function submitQuestion()
             if (error) {
                 alert("Data could not be saved." + error);
             } else {
-                location.href = "questionsubmitted.html"
+              var updateData = firebase.database().ref("user");
+                updateData.on("child_added", function(data, prevChildKey) {
+                var data = data.val()
+                userArray.push(data)
+              })
+              //Searches up all of the users in the database
+              firebase.database().ref('Completed/').set("Data is saved", function(error) {
+                  if (error) {
+                      alert("Data could not be saved." + error);
+                  } else {
+                    for(var i=0;i<userArray.length;i++)
+                    {
+                      if(userArray[i].username == sessionStorage.getItem("BBTrivianame"))
+                      {
+                        //Finds the user that match
+                        previousNumberOfQuestionsSubmitted = userArray[i].questionsSubmitted
+                        identity = userArray[i].identification
+                        //Collects the identiciation key and the amount of questions that they have previously submitted
+                      }
+                    }
+                    var totalQuestions = previousNumberOfQuestionsSubmitted + 1
+                    var save = firebase.database().ref('user/' + identity).update({
+                        questionsSubmitted: totalQuestions
+                    });
+                    //Registers into the database the new data
+                    firebase.database().ref('Completed/').set("Data is saved", function(error) {
+                        if (error) {
+                            alert("Data could not be saved." + error);
+                        } else {
+                          location.href = "questionsubmitted.html"
+                          //Takes you to a new link after you have submitted your question
+                        }
+                    });
+                  }
+              });
+
+
             }
         });
-        //Takes you to a new link after you have submitted your question
+        
     }
     else if(questionName = "" || correctAnswer == "" || wrongAnswer1 == "" || wrongAnswer2 == "")
     {
