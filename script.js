448228c38a99e08a2574b79d5de141cf2e86a8c2
JRProgramming <ramirezjohnny@icloud.com>
2018-05-06 21:47:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-var isEligibleEmail = true;
-
 function register(){
   var name = document.getElementById("nameTextField").value
-  var email = document.getElementById("schoolEmailTextField").value
-  if(name != "" && email != "")
+  if(name != "")
   {
     //Confirming to see if the user typed in both of the textboxes
     var array = [];
@@ -20,81 +17,49 @@ function register(){
         var isAMember = false
           for(var i=0;i<array.length;i++)
           {
-            if(array[i].username == name && array[i].email == email)
+            if(array[i].username == name)
             {
               isAMember = true
             }
           }
           if(isAMember != true)
           {
-            var reversedBlindBrook = "gro.koorbdnilb"
-            var reversedEmail = ""
-            for(i=0;i<email.length;i++)
-            {
-              reversedEmail = email[i] + reversedEmail
-            }
-            for(i=0;i<14;i++)
-            {
-              if(reversedEmail[i] != reversedBlindBrook[i])
-              {
-                isEligibleEmail = false
-              }
-            }
-            if(isEligibleEmail != false)
-            {
-              var save = firebase.database().ref('user/').push({
-                  username: name,
-                  email: email,
-                  eligible: true,
-                  questionsAnswered: 0,
-                  questionsSubmitted: 0,
-                  totalQuestions: 0,
-                  gamesWon: 0,
-                  totalGames: 0
-              });
-              var id = save.key
-              firebase.database().ref('user/' + id).update({
-                identification: id
-              })
-              firebase.database().ref('Completed/').set("Data is saved", function(error) {
-                  if (error) {
-                      alert("Data could not be saved." + error);
-                  } else {
-                    sessionStorage.setItem("login", true)
-                    location.href = "homepage.html"
-                  }
-              });
-            }
-            else {
-              alert("I'm sorry but this website only allows people with Blind Brook emails to have access to this site.")
-              name = ""
-              email = ""
-            }
+            var save = firebase.database().ref('user/').push({
+                username: name,
+                eligible: true,
+                questionsAnswered: 0,
+                totalQuestions: 0,
+                gamesWon: 0,
+                totalGames: 0,
+                questionsSubmitted: 0,
+                gamesLeft: 3
+            });
+            var id = save.key
+            firebase.database().ref('user/' + id).update({
+              identification: id
+            })
+            firebase.database().ref('Completed/').set("Data is saved", function(error) {
+                if (error) {
+                    alert("Data could not be saved." + error);
+                } else {
+                  sessionStorage.setItem("login", true)
+                  location.href = "homePage.html"
+                }
+            });
           }
           else
           {
             //The user already has an account right here.
-            location.href = "homepage.html"
+            location.href = "homePage.html"
           }
       }
     })
   }
   else
   {
-    if(name == "" && email == "")
+   if(name == "")
     {
-      //Checks to see if both of the textfields are filled in
-      alert("Both textfields are empty. Please type in your full name and your email.")
-    }
-    else if(name == "")
-    {
-      //Will send an alert if the name textfield is blank
-      alert("You didn't type in your name. Please type in your first and last name.")
-    }
-    else
-    {
-      //Will send an alert if the email textfield is blank
-      alert("You didn't type in your school email. Please do that right now.")
+      alert("Please enter your name.")
     }
   }
 }
